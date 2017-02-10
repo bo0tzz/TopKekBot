17c82741e1d88f4a8408d17897e5b76bc6498b7d
Nick Robson <robson.nicholas1@gmail.com>
2017-02-10 13:05:11 +1100

    Case sensitivity & remove debug

--- src/main/java/com/bo0tzz/topkekbot/TopKekListener.java
+++ src/main/java/com/bo0tzz/topkekbot/TopKekListener.java
@@ -2,6 +2,7 @@ package com.bo0tzz.topkekbot;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.SecureRandom;
 import java.util.Objects;
 import java.util.regex.Pattern;
 import pro.zackpollard.telegrambot.api.TelegramBot;
@@ -61,15 +62,14 @@ public class TopKekListener implements Listener {
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("*sigh*") && ev.getMessage().getSender().getUsername().equalsIgnoreCase("zackpollard"),
                     (e) -> "Yes yes Zack, we get it, you're sick of our shit."));
             add(new TextAction((t, ev) -> t.toLowerCase().contains("girl") && ev.getMessage().getSender().getUsername().equalsIgnoreCase("MazenK"), (e) -> "April is watching..."));
-            add(new TextAction((t, ev) -> t.toLowerCase().contains("xd"), (e) -> {
+            add(new TextAction((t, ev) -> t.contains("xD"), (e) -> {
                 String s = e.getContent().getContent().toLowerCase();
                 int index = -1;
                 String m = "";
+                Random r = new SecureRandom(); // we don't want people guessing!!!
                 while ((index = s.indexOf("xd", index + 1)) != -1) {
-                    System.out.println('a');
-                    Random r = new Random();
                     m += "x";
-                    for (int i = 0; i < r.nextInt(10); i++) {
+                    for (int i = 0; i < r.nextInt(16); i++) {
                         m += xd[r.nextInt(4)];
                     }
                     m += "D";
