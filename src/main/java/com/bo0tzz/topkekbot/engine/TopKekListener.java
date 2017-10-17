91519dc65472ad2372ec4c4f5334d1faa88d6c39
Amir Omidi <amir@aaomidi.com>
2017-10-17 10:35:58 -0400

    Does this fix it?1

--- src/main/java/com/bo0tzz/topkekbot/engine/TopKekListener.java
+++ src/main/java/com/bo0tzz/topkekbot/engine/TopKekListener.java
@@ -64,7 +64,7 @@ public class TopKekListener implements Listener {
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("sup"), (e) -> "hi"));
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("oh canada"), (e) -> "http://i.imgur.com/bULAfzE.jpg"));
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("fat"), (e) -> "Amir is fat"));
-            add(new TextAction((t, ev) -> t.equalsIgnoreCase("fish go moo"), (e) -> "@TopKek_Bot notices that " + e.getMessage().getSender().getFullName() + " is truly enlightened."));
+            add(new TextAction((t, ev) -> t.equalsIgnoreCase("fish go moo"), (e) -> "TopKek_Bot notices that " + e.getMessage().getSender().getFullName() + " is truly enlightened."));
             add(new TextAction((t, ev) -> t.toLowerCase().endsWith("go moo"), (e) -> "What are you, fucking retarded?"));
             add(new TextAction((t, ev) -> t.contains("@topkek_bot"), (e) -> "topkek"));
             add(new TextAction((t, ev) -> t.toLowerCase().contains("trident"), (e) -> secureRandom.nextInt(100) <= 15 ? "TridnetSDK is dead." : null));
