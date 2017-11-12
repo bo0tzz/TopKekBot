d94b1c7217b8a30e36fe4591beb48240e1ae80a0
bo0tzz <btdewilligen@gmail.com>
2017-11-12 16:33:09 +0100

    Update TopKekListener.java

--- src/main/java/com/bo0tzz/topkekbot/engine/TopKekListener.java
+++ src/main/java/com/bo0tzz/topkekbot/engine/TopKekListener.java
@@ -63,7 +63,7 @@ public class TopKekListener implements Listener {
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("hi"), (e) -> "sup"));
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("sup"), (e) -> "hi"));
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("oh canada"), (e) -> "http://i.imgur.com/bULAfzE.jpg"));
-            add(new TextAction((t, ev) -> t.equalsIgnoreCase("fat"), (e) -> "Amir is fat"));
+            add(new TextAction((t, ev) -> t.equalsIgnoreCase("fat"), (e) -> "Leo is fat"));
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("fish go moo"), (e) -> SendableTextMessage.plain("@TopKek_Bot notices that " + e.getMessage().getSender().getFullName() + " is truly enlightened.").build(), false));
             add(new TextAction((t, ev) -> t.toLowerCase().endsWith("go moo"), (e) -> "What are you, fucking retarded?"));
             add(new TextAction((t, ev) -> t.contains("@topkek_bot"), (e) -> "topkek"));
