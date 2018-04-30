29120b73572cda4046d4c97e352e7f9ef21e758a
Bo0tzz <btdewilligen@gmail.com>
2018-04-30 21:15:10 +0200

    Add default sendMessage

--- src/main/java/com/bo0tzz/topkekbot/engine/event/text/AbstractTextHandler.java
+++ src/main/java/com/bo0tzz/topkekbot/engine/event/text/AbstractTextHandler.java
@@ -2,6 +2,7 @@ package com.bo0tzz.topkekbot.engine.event.text;
 
 import com.jtelegram.api.TelegramBot;
 import com.jtelegram.api.chat.id.ChatId;
+import com.jtelegram.api.requests.message.framework.ParseMode;
 import com.jtelegram.api.requests.message.send.SendText;
 
 public abstract class AbstractTextHandler implements TextHandler {
@@ -17,13 +18,18 @@ public abstract class AbstractTextHandler implements TextHandler {
 
     /**
      * Send a message in reply to an event
-     * @param chat
-     * @param message
-     * @param markdown
+     * @param chat The chat to send the message to
+     * @param message The message to send
+     * @param parseMode The ParseMode to use
      */
-    private void sendMessage(ChatId chat, String message, boolean markdown) {
+    private void sendMessage(ChatId chat, String message, ParseMode parseMode) {
 
+        SendText.SendTextBuilder builder = SendText.builder()
+                .chatId(chat)
+                .text(message)
+                .parseMode(ParseMode.MARKDOWN);
 
+        bot.perform(builder.build());
 
     }
 
