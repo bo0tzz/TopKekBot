dbdc60e3e69fea3e4ca9b18955507266d43967e5
Vilsol <Vilsol@users.noreply.github.com>
2018-06-21 04:51:26 +0300

    More accurate

--- src/main/java/com/bo0tzz/topkekbot/engine/TopKekListener.java
+++ src/main/java/com/bo0tzz/topkekbot/engine/TopKekListener.java
@@ -65,7 +65,7 @@ public class TopKekListener implements Listener {
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("no u"), (e) -> NO_U.next()));
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("sup"), (e) -> "hi"));
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("oh canada"), (e) -> "http://i.imgur.com/bULAfzE.jpg"));
-            add(new TextAction((t, ev) -> t.equalsIgnoreCase("fat"), (e) -> "Vilsol is fat"));
+            add(new TextAction((t, ev) -> t.equalsIgnoreCase("fat"), (e) -> "Night is fat"));
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("skinny"), (e) -> "Fatass"));
             add(new TextAction((t, ev) -> t.equalsIgnoreCase("fish go moo"), (e) -> SendableTextMessage.plain("@TopKek_Bot notices that " + e.getMessage().getSender().getFullName() + " is truly enlightened.").build(), false));
             add(new TextAction((t, ev) -> t.toLowerCase().endsWith("go moo"), (e) -> "What are you, fucking retarded?"));
